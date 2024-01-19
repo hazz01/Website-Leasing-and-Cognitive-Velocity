32c1a879059106ce33c780d2dddc2446b8316d19
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-01-20 00:05:42 +0700

    add Timer frame 18, 19, 20, 21

--- dist/frame-group-1/time.js
+++ dist/frame-group-1/time.js
@@ -1,25 +1,37 @@
+let timeStart = 60; // Time Start
 function startTimer(duration, display) {
-  var timer = duration,
-    minutes,
-    seconds;
-  setInterval(function () {
+  var timer = duration;
+  var minutes, seconds;
+  let warning = timeStart;
+  var ok = false;
+  var form = document.forms.exam;
+
+  var t = setInterval(function () {
     minutes = parseInt(timer / 60, 10);
     seconds = parseInt(timer % 60, 10);
-
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
     display.textContent = minutes + ":" + seconds;
 
-    if (--timer < 0) {
-      timer = 0;
-      timer = duration; // uncomment this line to reset timer automatically after reaching 0
+    if (timer <= warning && !ok) {
+      alert("Time Is Running");
+      ok = true;
+    }
+
+    if (--timer <= 0) {
+      timer = duration;
+
+      if (!isNaN(t)) clearInterval(t);
+
+      window.location.href = "end.html";
+      form.submit();
     }
   }, 1000);
 }
 
 window.onload = function () {
-  var time = 60 / 2, // your time in seconds here
+  var time = timeStart, // your time in seconds here
     display = document.querySelector("#TimerDisplay");
   startTimer(time, display);
 };
