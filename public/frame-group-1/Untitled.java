11f09e157be55f0ddc4ecad9ff3465050879320c
Haza Nasrullah <hazanasrullah@gmail.com>
2024-01-30 09:29:39 +0700

    add frame value page

---
+++ public/frame-group-1/Untitled.java
@@ -0,0 +1,7 @@
+public class Untitled {
+    public static void main(String[] args) {
+        // dasuofhcsud
+        /*int Pikek;
+        int Daniel;*/
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}", "./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./public/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
